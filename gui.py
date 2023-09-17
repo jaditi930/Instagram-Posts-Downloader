e2c58b1bce988e431922a6b9f37842a331b38061
Aditi Jain <jaditi930@gmail.com>
2023-09-17 23:49:09 +0530

    downloads queued

--- gui.py
+++ gui.py
@@ -4,41 +4,51 @@ ig = instaloader.Instaloader()
 from tkinter import *
 from time import sleep
 
+#https://www.instagram.com/reel/CxPwnKxMcvg/?utm_source=ig_web_copy_link&igshid=MzRlODBiNWFlZA==
 downloading=0 
 queue=[]
 
 def download():
+    global downloading
+    downloading=1
     while len(queue)>0:
         item=queue.pop()
         user_input=item[0]
         type=item[1]
+        print(user_input,type)
 
+        try:
         # to download profile picture
-        if type==0:
-            ig.download_profile(user_input , profile_pic_only=True)
+            if type==0:
+                ig.download_profile(user_input , profile_pic_only=True)
 
-        # to download particular post,video or reel
-        elif type==1:
-            post = instaloader.Post.from_shortcode(ig.context,user_input)
-            ig.download_post(post,target="downloads")
+            # to download particular post,video or reel
+            elif type==1:
+                shortcode=user_input.split("/")[4]
+                post = instaloader.Post.from_shortcode(ig.context,shortcode)
+                ig.download_post(post,target="downloads")
 
-        # to download all posts of a user
-        else:
-            profile = instaloader.Profile.from_username(ig.context,user_input)
+            # to download all posts of a user
+            else:
+                profile = instaloader.Profile.from_username(ig.context,user_input)
 
-            for post in profile.get_posts():
-                print(post.title)
-                ig.download_post(post, target=profile.username)
+                for post in profile.get_posts():
+                    print(post.title)
+                    ig.download_post(post, target=profile.username)
 
+            dwld_label.config(text="Downloading has completed successfully ...")
 
-    dwld_label.config(text="Downloading has completed successfully ...")
+        except :
+            dwld_label.config(text="Downloading failed ...")
+
     sleep(1)
     dwld_label.config(text="Ready to Download ...")
+    downloading=0
 
 
-def start_download(type):
+def start_download():
 
-    queue.append((Userinput.get(),type))
+    queue.append((Userinput.get(),Type.get()))
     if downloading==0:
         t1 = threading.Thread(target=download, args=())
         t1.start()
@@ -48,9 +58,8 @@ def start_download(type):
 
 
 def update_gui(type):
+    Type.set(type)
     title.config(text=choice_list[type])
-    entry.config(textvariable=Userinput)
-    dwld_btn.config(command=lambda:start_download(type))
 
     if type==1:
         label.config(text="Enter url:")
@@ -58,24 +67,22 @@ def update_gui(type):
     else:
         label.config(text="Enter username:")
 
-    label.pack(in_=f2,side=LEFT,anchor=W)
-    entry.pack(in_=f2,side=RIGHT,anchor=W)
-    dwld_btn.pack(in_=f3,fill=X)
-    
-
 window=Tk()
 window.geometry("944x634")
+window.minsize(944,634)
+window.maxsize(944,634)
 window.title("Instagram Posts Downloader")
 
 Userinput=StringVar()
+Type=IntVar()
 
-text=Label(text="INSTAGRAM POSTS DOWNLOADER",font=("Helventica", "20", "bold"),fg="black",padx=10,pady=5)
+text=Label(text="INSTAGRAM POSTS DOWNLOADER",font=("Helventica", "25", "bold"),fg="black",padx=10,pady=20)
 text.pack()
 
 choice_list=["DOWNLOAD PROFILE PHOTO","DOWNLOAD A POST, VIDEO OR REEL","DOWNLOAD ALL POSTS"]
 
 f1=Frame()
-f1.pack(side=TOP)
+f1.pack()
 
 b1=Button(text=choice_list[0],font=("Helventica", "12", "bold"),padx=5,pady=5,command=lambda:update_gui(0))
 b2=Button(text=choice_list[1],font=("Helventica", "12", "bold"),padx=5,pady=5,command=lambda:update_gui(1))
@@ -85,21 +92,27 @@ b1.pack(in_=f1, side=LEFT)
 b2.pack(in_=f1, side=LEFT)
 b3.pack(in_=f1, side=LEFT)
 
-title=Label(text="",font=("Times New Roman", "15","italic"),padx=15,pady=15)
-title.pack(anchor=W)
+title=Label(text=choice_list[Type.get()],font=("Helventica", "16","bold"),padx=15,pady=15,width=100)
+title.pack()
 
 f2=Frame()
-f2.pack()
+f2.pack(ipadx=20,ipady=20)
 
+label=Label(text="Enter username:",font=("Helventica", "15"))
+entry=Entry(font=("Helventica", "15"),width=40,textvariable=Userinput)
+
+label.pack(in_=f2,side=LEFT,anchor=W)
+entry.pack(in_=f2,side=RIGHT,anchor=W)
 
 f3=Frame()
 f3.pack()
 
-label=Label(text="",font=("Helventica", "15"))
-entry=Entry(font=("Helventica", "15"),width=40)
-dwld_btn=Button(text="Download",bg="light green",padx=5,pady=5,font=("15"),width=50)
+dwld_btn=Button(text="DOWNLOAD",bg="light green",padx=5,pady=5,font=("Helventica","15"),width=20,command=start_download)
+dwld_btn.pack(in_=f3)
 
-dwld_label=Label(text="Ready to Download ...",font=("Helventica", "15"))
-dwld_label.pack(ipadx=5,ipady=5)
+f4=Frame()
+f4.pack(side=BOTTOM,anchor="sw")
+dwld_label=Label(text="Ready to Download ...",font=("Helventica", "15"),width=100,bg="sky blue")
+dwld_label.pack(in_=f4,ipadx=5,ipady=10)
  
 window.mainloop()
